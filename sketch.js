484ee2468150031a22221850109026c9a0a6ea39
Sargun-Juneja <69464071+Sargun-Juneja@users.noreply.github.com>
2020-10-16 17:56:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var gameState = 0;
+var database;
+var playerCount;
+var form,player,game;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+    console.log(database);
+
+    game = new Game();
+    game.getState();
+    game.start()
+}
+
+function draw(){
+    background("white");
+
+    if(playerCount === 4) {
+        game.update(1)
+    }
+
+    if(gameState ===1) {
+        clear();
+        game.play();
+    }
+}
+
